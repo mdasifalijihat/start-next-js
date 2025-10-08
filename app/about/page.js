f7fb0602006763bcbe14b95cb2d6e460adad3530
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-08 12:34:53 +0600

    added about pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function page() {
+export default function about() {
   return (
     <div>
-      <h2>hello world</h2>
+      <h2>about page</h2>
     </div>
   )
 }
