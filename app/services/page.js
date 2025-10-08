123b5163825c3c86f8d09ded22e440e3d4de123e
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-08 12:39:40 +0600

    added servicess

---
+++ app/services/page.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function  services() {
+  return (
+    <div>
+      <h2>services </h2>
+      <Link href={'/about'}> About </Link>
+    </div>
+  )
+}
