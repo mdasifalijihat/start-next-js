1b11b8aff1593b332576e8794cc5a1032de0e42a
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-09 22:34:43 +0600

    added about pages

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function about() {
   return (
     <div>
       <h2>about page</h2>
+      <Link href={'/about'}> About </Link>
     </div>
   )
 }
--- app/services/page.js
+++ app/services/page.js
@@ -9,3 +9,4 @@ export default function  services() {
     </div>
   )
 }
+6
