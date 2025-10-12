3f79b5b5bceaf677e91ab09a7e8bd6748922822f
mdasifalijihat <mdasifalijihat@gmail.com>
2025-10-12 09:56:36 +0600

    added link

--- app/services/local/page.js
+++ app/services/local/page.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
       local age 
+      <Link href="/local/page2">go to page2</Link>
     </div>
   )
 }
